cc70036fc54f6abb15457a2a2724ee3279f253b0
Russ Biggs <russbiggs@gmail.com>
2025-07-25 10:33:53 -0600

    add nolint

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -2,7 +2,7 @@ library("vcr")
 
 vcr_dir <- vcr::vcr_test_path("fixtures")
 
-# set_base_url('http://localhost:8000') # uncomment when generating vcr cassettes to point test cases to local DB
+# set_base_url('http://localhost:8000') # nolint uncomment when generating vcr cassettes to point test cases to local DB
 
 if (!nzchar(Sys.getenv("OPENAQ_API_KEY"))) {
   if (dir.exists(vcr_dir)) {
